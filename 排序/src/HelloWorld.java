fba0e715616be55cac98a1eec852c3b8b63be096
Mr.Bai <3059446309@qq.com>
2022-03-25 19:19:29 +0800

    HelloWorld firstCommit

---
+++ 排序/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @Author baixu
+ * @Date 2022/3/25 0025 19:18
+ * Version 1.0
+ * @Description :
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World1");
+    }
+}
